04d0c208d0cd20fe36f2b1776173e34a7f7be361
Marcos Marado <mindboosternoori@gmail.com>
2017-03-21 00:41:06 +0000

    Check that an argument was provided

--- rpp.sh
+++ rpp.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+    then echo "No arguments supplied. Please provide an URL."
+    exit
+fi
+
 mplayer $(wget $1 -o /dev/null -O -|grep hls_url |cut -d: -f2-|cut -d\" -f2)
